0b4b99f5cac3539ea2ddffef502dffe760e4f28a
Christian SHUNGU <christianshungu@gmail.com>
2021-10-07 20:28:18 +0200

    Ajout de Webpack Encore

---
+++ assets/app.ts
@@ -0,0 +1,14 @@
+/**
+    * @description      : Fichier principal
+    * @author           : christian
+    * @group            : 
+    * @created          : 06/10/2021 - 00:54:31
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 06/10/2021
+    * - Author          : christian
+    * - Modification    : 
+**/
+import "./sass/app.scss";
+import "./ts/video/video.ts";
---
+++ assets/ts/video/video.ts
@@ -0,0 +1,15 @@
+/**
+    * @description      : Video animate Thumbnail
+    * @author           : christian
+    * @group            : 
+    * @created          : 07/10/2021 - 20:17:33
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 07/10/2021
+    * - Author          : christian
+    * - Modification    : 
+**/
+const Video: any = (function () {
+    "use strict";
+})();
